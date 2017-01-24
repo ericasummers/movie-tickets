96c1969d5e7f5c9ec39fcf2558810df9800184f7
andrew devlin and Erica Wright <andrew@>
2017-01-24 15:06:08 -0800

    add javascript for pricing depending on age and time-work-in-progress

---
+++ README.md
@@ -0,0 +1,69 @@
+
+Age limits:
+If under 18, half off
+Certain movies cannot be seen by under 18s
+Matinee special
+If over 65, half off
+
+
+Prices:
+Snow White
+adult:
+6am = $5
+10am = $5
+12pm = $8
+3pm = $10
+5pm = $12
+8pm = $14
+matinee = $7
+
+
+Aladdin
+adult:
+6am = $5
+10am = $5
+12pm = $8
+3pm = $10
+5pm = $12
+8pm = $14
+matinee = $7
+
+Steamboat Willy
+adult:
+6am = $5
+10am = $5
+12pm = $8
+3pm = $10
+5pm = $12
+8pm = $14
+matinee = $7
+
+The Lion King
+adult:
+6am = $5
+10am = $5
+12pm = $8
+3pm = $10
+5pm = $12
+8pm = $14
+matinee = $7
+
+Frozen
+adult:
+6am = $5
+10am = $5
+12pm = $8
+3pm = $10
+5pm = $12
+8pm = $14
+matinee = $7
+
+Chitty Chitty Bang Bang
+adult:
+6am = $5
+10am = $5
+12pm = $8
+3pm = $10
+5pm = $12
+8pm = $14
+matinee = $7
--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,33 @@ function MoviePrices(name, age) {
   this.movieTime = [];
 }
 
+function priceByTime() {
+  newMoviePrices.movieTime.forEach(function(time) {
+    if (inputtedMovieTime === "6AM" || inputtedMovieTime === "10AM") {
+      timePrice = 5;
+    } else if (inputtedMovieTime === "Matinee") {
+      return 7;
+    } else if (inputtedMovieTime === "12PM") {
+      return 10;
+    } else if (inputtedMovieTime === "3PM") {
+      return 10;
+    } else if (inputtedMovieTime === "5PM") {
+      return 12;
+    } else if (inputtedMovieTime === "8PM") {
+      return 14;
+    }
+  });
+};
+
+var newMoviePrices.movieTime;
+var inputtedMovieAge;
+var output = ''
+function pricePerAge(priceByTime) {
+  if (inputtedMovieAge < 18 || inputtedMovieAge > 65) {
+    return '<li>' + inputtedMovieTime + (priceByTime / 2) + '</li>';
+  }
+}
+
 //front end
 
 $(document).ready(function() {
@@ -12,9 +39,8 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputtedMovieName = $("#movies").val();
-    var inputtedMovieAge = $("input#age").val();
+    var inputtedMovieAge = parseInt($("input#age").val());
     var newMoviePrices = new MoviePrices(inputtedMovieName, inputtedMovieAge);
-
 
     var inputtedMovieTime = $("input:checkbox[name=times-of-day]:checked").each(function() {
       var timesOfDay = $(this).val();
@@ -30,6 +56,10 @@ $(document).ready(function() {
 
     $("#pricing-results").show();
 
+    for (var i = 0; i < newMoviePrices.movieTime.length; i++)
+    $("#pricing").append(pricePerAge(priceByTime(newMoviePrices.movieTime)));
+
+    $("#pricing-answers").show();
 
   });
 });
